9d436e4ab2fdb75a3c6d9c89ce8b1559e9013431
Ryan McCormick <ryan@dupperanalytics.com>
2023-11-10 22:51:07 -0700

    add animation when route is changed, can be easily replaced with an image or more complex animation, this just gives us the core logic

--- webmaster-2024/src/App.js
+++ webmaster-2024/src/App.js
@@ -1,19 +1,87 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { useState, useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
+// import './App.css';
+// import Frontpage from './Frontpage';
+// import Header from './Header';
+// import SlideShow from './Slideshow';
+// import TransitionAnimation from './TransitionAnimation';
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Header/>
+//         <Routes>
+//           <Route path="/" element={<Frontpage />} />
+//           <Route path="/slideshow" element={<SlideShow />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Frontpage from './Frontpage';
 import Header from './Header';
 import SlideShow from './Slideshow';
+import TransitionAnimation from './TransitionAnimation';
+
+function AppContent() {
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState(location.pathname);
+  const [prevPath, setPrevPath] = useState(null);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    if (location.pathname !== currentPath) {
+      setIsAnimating(true);
+      setPrevPath(currentPath); // Save the previous path
+
+      // Timer to update the component (halfway through the animation)
+      setTimeout(() => {
+        setCurrentPath(location.pathname);
+        setPrevPath(null); // Reset the previous path
+      }, 1000); // half duration
+
+      // Timer to end the animation (at the end of the animation)
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 2000); // full duration
+    }
+  }, [location, currentPath]);
+
+  const renderComponentForPath = (path) => {
+    switch (path) {
+      case '/':
+        return <Frontpage />;
+      case '/slideshow':
+        return <SlideShow />;
+      default:
+        return <Frontpage />; // Default case
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      {isAnimating && <TransitionAnimation />}
+      {!prevPath && renderComponentForPath(currentPath)}
+      {isAnimating && prevPath && renderComponentForPath(prevPath)}
+    </div>
+  );
+}
+
+
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Frontpage />} />
-          <Route path="/slideshow" element={<SlideShow />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
 }
---
+++ webmaster-2024/src/TransitionAnimation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TransitionAnimation.css';
+
+const TransitionAnimation = () => {
+  return (
+    <div className="transition-animation"></div>
+  );
+};
+
+export default TransitionAnimation;
