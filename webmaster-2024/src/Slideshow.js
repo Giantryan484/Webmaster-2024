e2e7fd0b2f64d631be9b92e1f950ff671173a3c3
Ryan McCormick <ryan@dupperanalytics.com>
2023-12-04 09:56:25 -0700

    make the flashcards prettier and animate differently

--- webmaster-2024/src/Slideshow.js
+++ webmaster-2024/src/Slideshow.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Slideshow.css'; // Import your CSS file
 
 const slides = [
@@ -6,7 +6,7 @@ const slides = [
     { background: 'linear-gradient(to right, #00b09b, #96c93d)' },
     { background: 'linear-gradient(to top, #fbc2eb, #a6c1ee)' },
     { background: 'linear-gradient(to right, #f83600, #f9d423)' },
-    { background: 'linear-gradient(to right, #4e54c8, #8f94fb)' },
+    { background: 'linear-gradient(to right, #000000, #ffffff)' },
     { background: 'linear-gradient(to top, #ff758c, #ff7eb3)' },
     { background: 'linear-gradient(to right, #36d1dc, #5b86e5)' },
     { background: 'linear-gradient(to right, #ee9ca7, #ffdde1)' },
@@ -15,30 +15,50 @@ const slides = [
 ];
   
 
-function Slideshow({currentSlide, setCurrentSlide}) {
-    const [slideDirection, setSlideDirection] = useState('forward');
+function Slideshow({ currentSlide, setCurrentSlide }) {
+  const [slideDirection, setSlideDirection] = useState('forward');
+  const [animating, setAnimating] = useState(false);
+  const [animatedSlide, setAnimatedSlide] = useState(null);
 
-    const goBack = () => {
-        setSlideDirection('backward');
+  async function goBack() {
+    if (currentSlide > 0) {
+      setSlideDirection('backward');
+      setAnimatedSlide(currentSlide - 1)
+      setAnimating(true);
+      setTimeout(() => {
         setCurrentSlide(currentSlide - 1);
-    };
+      }, 499);
+    }
+  };
 
-    const goForward = () => {
-        setSlideDirection('forward');
-        setCurrentSlide(currentSlide + 1);
-    };
-  
-    return (
-      <div className='slideshow-page'>
-        <div className="slideshow-container">
+  async function goForward() {
+    if (currentSlide < slides.length - 1) {
+      setSlideDirection('forward');
+      setAnimatedSlide(currentSlide)
+      setAnimating(true);
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (animating) {
+      const timeout = setTimeout(() => setAnimating(false), 500);
+      return () => clearTimeout(timeout);
+    }
+  }, [animating]);
+
+  return (
+    <div className='slideshow-page'>
+      <div className="slideshow-container">
         {currentSlide > 0 && <button className="arrow-button go-back" onClick={goBack}>←</button>}
-            <div className={`slide ${slideDirection}`} style={slides[currentSlide]}>
-                {/* Slide content */}
-            </div>
+        <div className='slide slide-main' style={slides[currentSlide]}></div>
+        {animating && <div className={`slide ${slideDirection === 'forward' ? 'sliding-off' : 'sliding-on'}`}
+             style={slides[animatedSlide]}></div>
+        }
         {currentSlide < slides.length - 1 && <button className="arrow-button go-forward" onClick={goForward}>→</button>}
-    </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Slideshow;
