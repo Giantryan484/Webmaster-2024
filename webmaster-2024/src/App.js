da8926bb2a1c726ce425e7f607698d6e55f16c7d
Ryan McCormick <ryan@dupperanalytics.com>
2023-12-05 19:34:46 -0700

    try new tree layout

--- webmaster-2024/src/App.js
+++ webmaster-2024/src/App.js
@@ -1,103 +1,25 @@
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
-// import './App.css';
-// import Frontpage from './Frontpage';
-// import Header from './Header';
-// import SlideShow from './Slideshow';
-// import TransitionAnimation from './TransitionAnimation';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Header/>
-//         <Routes>
-//           <Route path="/" element={<Frontpage />} />
-//           <Route path="/slideshow" element={<SlideShow />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-import { BrowserRouter as Router } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import './App.css';
-import Frontpage from './Frontpage';
-import Header from './Header';
-import SlideShow from './Slideshow';
-import TransitionAnimation from './TransitionAnimation';
-import Resources from './Resources';
-import Explorer from './Explorer';
-import Presentation from './Presentation';
-
-function AppContent() {
-  const location = useLocation();
-  const [currentPath, setCurrentPath] = useState(location.pathname);
-  const [prevPath, setPrevPath] = useState(null);
-  const [isAnimating, setIsAnimating] = useState(false);
-
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  useEffect(() => {
-    if (isAnimating) {
-      return;
-    }
-
-    if (location.pathname !== currentPath) {
-      setIsAnimating(true);
-      setPrevPath(currentPath); // Save the previous path
-
-      // Timer to update the component (halfway through the animation)
-      setTimeout(() => {
-        setCurrentPath(location.pathname);
-        setPrevPath(null); // Reset the previous path
-      }, 1000); // half duration
-
-      // Timer to end the animation (at the end of the animation)
-      setTimeout(() => {
-        setIsAnimating(false);
-      }, 2000); // full duration
-    }
-  }, [location, currentPath, isAnimating]);
-
-  const renderComponentForPath = (path) => {
-    switch (path) {
-      case '/':
-        return <Frontpage />;
-      case '/slideshow':
-        return <SlideShow currentSlide={currentSlide} setCurrentSlide={setCurrentSlide}/>;
-      case '/resources':
-        return <Resources />;
-      case '/home-explorer':
-        return <Explorer />;
-      case '/how-to-make-big-change':
-        return <Presentation />;
-      default:
-        return <Frontpage />; // Default case
-    }
-  };
-
-  return (
-    <div className="App">
-      <Header />
-      {isAnimating && <TransitionAnimation />}
-      {!prevPath && renderComponentForPath(currentPath)}
-      {isAnimating && prevPath && renderComponentForPath(prevPath)}
-    </div>
-  );
-}
-
-
 
 function App() {
+  const handleScroll = useCallback((e) => {
+    const scrollY = e.target.scrollTop;
+
+    // Adjust these values based on how you want the rectangles to move
+    document.getElementById('rectangle1').style.top = (10 + scrollY * 0.1) + '%';
+    document.getElementById('rectangle2').style.top = (30 + scrollY * 0.1) + '%';
+    document.getElementById('rectangle3').style.top = (50 + scrollY * 0.1) + '%';
+  }, []);
+
   return (
-    <Router>
-      <AppContent />
-    </Router>
+    <div className="scroll-container">
+      <div className="svg-container">
+        <img src={`${process.env.PUBLIC_URL}/tree.svg`} alt="tree background" />
+      </div>
+      <div className="rectangle" id="rectangle1"></div>
+      <div className="rectangle" id="rectangle2"></div>
+      <div className="rectangle" id="rectangle3"></div>
+    </div>
   );
 }
 
