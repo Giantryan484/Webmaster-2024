431eb3dcaaf6a8b6c3e4bce39b2c726d6c483276
Ryan McCormick <ryan@dupperanalytics.com>
2023-12-07 18:31:00 -0700

    finalize zoom functionality and add placeholder slideshow

--- webmaster-2024/src/App.js
+++ webmaster-2024/src/App.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Flashcard from './Slideshow';
+import Slideshow from './Presentation';
 import Explorer from './Explorer';
 
 function App() {
   const [slide, setSlide] = useState(0);
 
+  const [zoomedIn, setZoomedIn] = useState(false);
   const [zoomLevel, setZoomLevel] = useState(1); // 1 for normal, greater for zoom
   const [transformOrigin, setTransformOrigin] = useState('left'); // 'left' or 'right'
 
@@ -17,14 +19,16 @@ function App() {
     // Scale the page after scrolling
     setTimeout(() => {
       // const transformOriginX = (scaleLevel > 1) ? 'right' : 'left'; // Choose 'left' or 'right' based on your requirement
+      setZoomedIn(true)
       setTransformOrigin(origin);
       setZoomLevel(scaleLevel);
     }, 300); // Adjust timeout based on scroll duration
   };
 
-  const handleResetZoom = (origin) => {
-    setTransformOrigin(origin);
+  const handleResetZoom = () => {
+    // setTransformOrigin(origin);
     setZoomLevel(1);
+    setZoomedIn(false);
     // window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
@@ -36,43 +40,62 @@ function App() {
       <div className="svg-background-container" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/tree.svg)` }}>
       <header className="header-bar">
               <div className="button-group left">
-                  <button className="header-button">Flashcards</button>
-                  <button className="header-button">Home Explorer</button>
+                  <button onClick={() => handleZoom(49, 2, "0vw 55vw")} className="header-button">Flashcards</button>
+                  <button onClick={() => handleZoom(79, 2, "100vw 82vw")} className="header-button">Home Explorer</button>
               </div>
               <div className="button-group right">
-                  <button className="header-button">Slideshow</button>
-                  <button className="header-button">Resources</button>
+                  <button onClick={() => handleZoom(105, 2, "0vw 105vw")} className="header-button">Slideshow</button>
+                  <button onClick={() => handleZoom(200, 1, "50vw 50vw")}className="header-button">Resources</button>
               </div>
           </header>
         <div className="rectangle top-left">
         <button onClick={() => handleZoom(49, 2, "0vw 55vw")}>Zoom into Section 1</button> {/* Replace 50 with actual position */}
-        <button onClick={() => handleResetZoom("0vw 55vw")}>Reset Zoom</button>
+        {/* <button onClick={() => handleResetZoom("0vw 55vw")}>Reset Zoom</button> */}
 
           <Flashcard currentSlide={slide} setCurrentSlide={setSlide}/>
         </div>
         <div className="rectangle middle-right">
         <button onClick={() => handleZoom(79, 2, "100vw 82vw")}>Zoom into Section 1</button> {/* Replace 50 with actual position */}
-        <button onClick={() => handleResetZoom("100vw 82vw")}>Reset Zoom</button>
+        {/* <button onClick={() => handleResetZoom("100vw 82vw")}>Reset Zoom</button> */}
           <Explorer />
         </div>
         <div className="rectangle bottom-left">
         <button onClick={() => handleZoom(105, 2, "0vw 105vw")}>Zoom into Section 1</button> {/* Replace 50 with actual position */}
-        <button onClick={() => handleResetZoom("0vw 105vw")}>Reset Zoom</button>
-          <iframe 
+        {/* <button onClick={() => handleResetZoom("0vw 105vw")}>Reset Zoom</button> */}
+          {/* <iframe 
                   src={`${process.env.PUBLIC_URL}/Webmaster sketches 12-4-23.pdf`}
                   title="PDF"
                   className="pdf-viewer"
-            ></iframe>
+          ></iframe> */}
+          <Slideshow />
+
         </div>
+
+        {zoomedIn === false && (
+          <>
+          <button className='big-button top-left-button' onClick={() => handleZoom(49, 2, "0vw 55vw")}></button>
+          <button className='big-button middle-right-button' onClick={() => handleZoom(79, 2, "100vw 82vw")}></button>
+          <button className='big-button bottom-left-button' onClick={() => handleZoom(105, 2, "0vw 105vw")}></button>
+          </>
+          // have these disappear when zoomed, then have clicking on the background reset zoom
+          // remove the origin from reset zoom, have it stay the same as what it was last set (just remove that one setTranform origin)
+          
+        )}
+        <div className='big-reset-button' onClick={() => handleResetZoom()}></div>
+
+
+        {/* eslint-disable-next-line */}
         <a className='resources-button CC' href='' target="_blank" rel="noopener noreferrer"></a>
+        {/* eslint-disable-next-line */}
         <a className='resources-button POWL' href='' target="_blank" rel="noopener noreferrer"></a>
+        {/* eslint-disable-next-line */}
         <a className='resources-button IMGS' href='' target="_blank" rel="noopener noreferrer"></a>
+        {/* eslint-disable-next-line */}
         <a className='resources-button LINKS' href='' target="_blank" rel="noopener noreferrer"></a>
+        {/* eslint-disable-next-line */}
         <a className='resources-button CODE' href='https://github.com/Giantryan484/Webmaster-2024' target="_blank" rel="noopener noreferrer"></a>
+        {/* eslint-disable-next-line */}
         <a className='resources-button BKGN' href='' target="_blank" rel="noopener noreferrer"></a>
-        <div className="content">
-          <h1></h1>
-        </div>
       </div>
     </div>
   );
--- webmaster-2024/src/Presentation.js
+++ webmaster-2024/src/Presentation.js
@@ -2,11 +2,26 @@ import React from 'react';
 import './Presentation.css';
 
 const Presentation = () => {
-    return (
-        <div>
-            This is placeholder text
-        </div>
-    );
-}
+  const images = [
+    `${process.env.PUBLIC_URL}/page_1.png`,
+    `${process.env.PUBLIC_URL}/page_2.png`,
+    `${process.env.PUBLIC_URL}/page_3.png`,
+    `${process.env.PUBLIC_URL}/page_4.png`,
+    `${process.env.PUBLIC_URL}/page_5.png`,
+    `${process.env.PUBLIC_URL}/page_6.png`,
+    `${process.env.PUBLIC_URL}/page_7.png`,
+    `${process.env.PUBLIC_URL}/page_8.png`,
+    `${process.env.PUBLIC_URL}/page_9.png`,
+    `${process.env.PUBLIC_URL}/page_10.png`,
+  ];
+
+  return (
+    <div className="pdf-viewer-container">
+      {images.map((image, index) => (
+        <img key={index} src={image} alt={`Page ${index + 1}`} />
+      ))}
+    </div>
+  );
+};
 
 export default Presentation;
